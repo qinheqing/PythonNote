9bb829796e301a73444357b0c589c53b48f7ccd0
qinheqing <qinheqing@gmail.com>
2017-12-13 00:19:17 +0800

    高阶函数
    filter

--- python/hightLevelFunction.py
+++ python/hightLevelFunction.py
@@ -49,10 +49,47 @@ print map(upper_frist,names)
 
 # reduce()函数其实是对与map()函数的补充，传入两个参数，进行迭代式的对list元素进行操作
 # reduce()还可以接收第3个可选参数，作为计算的初始值。如果把初始值设为100，计算：
-reduce(f, [1, 3, 5, 7, 9], 100)
+def f(x,y):
+    return x + y
+
+print reduce(f, [1, 3, 5, 7, 9], 100)
 
 # 测试：求积公式 输入 [2, 4, 5, 7, 12] 求结果
-def f1()
+def f1(x,y):
+    return x*y
+
 print reduce(f1,[2, 4, 5, 7, 12])
 
+#注意： python3中，要使用reduce,得从functools中引入，加上：from functools import reduce 才能够用。
 
+# 这个reduce函数有一个点比较好用，比如可以用它来吧字符串串起来，例如这样：
+def link_str(x,y):
+    return x + y
+
+print reduce(link_str,['Admin','abc','gfsddf'])
+
+# 3.0 python 中的高阶函数之filter()函数：
+# filter函数接受一个函数引用和一个list,这个函数f的作用是对每个list元素进行判断，返回True/False,filter()根据判断条件结果过滤掉不符合条件的元素，返回符合条件的元素
+def is_odd(x):
+    return x%2 ==1
+
+print filter(is_odd,[1,2,3,4,5,6,7,8,9])
+
+# 通过filter可以方便的处理很多事情，如删除None或者空字符串
+def not_empty_str(s):
+    return s and len(s.strip()) > 0
+
+print filter (not_empty_str,['abc','','asfd',None,'haha'])
+
+# 注意: s.strip(rm) 删除 s 字符串中开头、结尾处的 rm 序列的字符。当rm为空时，默认删除空白符（包括'\n', '\r', '\t', ' ')
+a = '     123'
+print a.strip()
+
+a='\t\t123\r\n'
+print a.strip()
+
+# 请利用filter()过滤出1~100中平方根是整数的数
+def is_sqr(x):
+    return math.sqrt(x)%1 == 0
+
+print filter(is_sqr, range(1, 101))
