82c6dc3a6d929227c415d01e0359a0232ea9a74d
qinheqing <qinheqing@gmail.com>
2017-12-18 23:26:07 +0800

    qhq

---
+++ python/import.py
@@ -0,0 +1,25 @@
+# python中的模板使用：
+# 要先使用一个模板，首先应该使用import关键字进行导入，例如，导入系统自带的模板math
+import math
+
+print math.pow(2,0.5)
+
+print math.pi
+
+# 如果我们只希望导入用到的math模板中的某几个函数，而不是所有的函数，可以使用如下语句：
+from math import pow,sin,log
+# 可以直接使用
+print pow(2,10)
+
+# 这样就可以使用pow,sin,log几个方法了
+# 导入的模块使用时需要以模块名引用，使用from xxx import ...导入的方法可能会引起名字冲突，可以使用别名
+from math import log
+from logging import log as Logger  # 这时可以直接使用logger
+
+# 动态的导入模块：
+try:
+    from cStringIO import StringIO
+Except ImportError:
+    from StringIO import StringIO
+
+
